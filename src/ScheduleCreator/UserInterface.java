da12e1f4814e100cd815e0d54f0bd444d7e36346
Christian Troyer <TroyerCP19@gcc.edu>
2022-03-08 22:00:03 -0500

    Displaying the options the user can on main menu.

--- src/ScheduleCreator/UserInterface.java
+++ src/ScheduleCreator/UserInterface.java
@@ -1,6 +1,5 @@
 package ScheduleCreator.ScheduleCreator;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class UserInterface {
@@ -8,33 +7,9 @@ public class UserInterface {
 	private static HashMap<String, Schedule> recommendedSchedules;
 	private static ArrayList<Course> courses;
 	private static Student currentStudent;
-
-	public static void consoleSearch() {
-		Scanner searchScan = new Scanner(System.in); //takes user input for now
-		System.out.println("Would you like to search by code (1) or keyword (2)?");
-		int searchType = searchScan.nextInt();
-		searchScan.nextLine();
-		if (searchType == 1) {
-			System.out.println("Searching by course code \nEnter the course code you would like to search for");
-
-			String code = searchScan.nextLine().toUpperCase(); //matches all to the format in the database
-			//check for valid user input
-			ArrayList<Course> searchResults = searchCoursesByCode(code);
-			System.out.println("Search results:");
-			for (int i = 1; i <= searchResults.size(); i++) {
-				System.out.println(i + ". " +searchResults.get(i - 1));
-			}
-		}
-	}
-	public static ArrayList<Course> searchCoursesByCode(String code) {
-		ArrayList<Course> result = new ArrayList<>();
-		for (Course c : courses) {
-			if (c.getCode().contains(code)) {
-				result.add(c);
-			}
-		}
-		return result;
-	}
+	private Scanner searchScan = new Scanner(System.in); //takes user input for now
+	
+	public ArrayList<Course> searchCoursesByCode(String code) {return null;}
 	public ArrayList<Course> searchCoursesByKeyword(String keyword) {return null;}
 	public ArrayList<Course> dayFilter(ArrayList<Course> courses, ArrayList<String> days) {return null;}
 	public ArrayList<Course> timeFilter(ArrayList<Course> courses, ArrayList<String> times) {return null;}
@@ -43,7 +18,28 @@ public class UserInterface {
 	public void createGuest() {}
 	public void viewSchedule(Schedule s) {}
 
+
 	public static void main(String args[]) {
+		Test test = new Test();
+		test.test1();
+		menuNav(0);
+	}
+
+	public static void menuNav(int menuId){
+		switch(menuId){
+			case 0:
+		}
+	}
+
+	public static void mainMenu(){
+		System.out.println("Welcome!");
+		System.out.println("1) Create an account");
+		System.out.println("2) Log into an account");
+		System.out.println("3) Continue as guest");
+	}
+
+	public static void createAccount(){
 
 	}
+
 }
