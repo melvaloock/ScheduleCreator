e005e4d74820808a0c3d08fbd4f4992a67116c3d
melvaloock <melvaloock@gmail.com>
2022-03-10 15:55:13 -0500

    UserInterface fixed

--- src/ScheduleCreator/UserInterface.java
+++ src/ScheduleCreator/UserInterface.java
@@ -9,8 +9,34 @@ public class UserInterface {
 	private static Student currentStudent;
 	private Scanner searchScan = new Scanner(System.in); //takes user input for now
 
-	
-	public ArrayList<Course> searchCoursesByCode(String code) {return null;}
+
+	public static void consoleSearch() {
+		Scanner searchScan = new Scanner(System.in); //takes user input for now
+		System.out.println("Would you like to search by code (1) or keyword (2)?");
+		int searchType = searchScan.nextInt();
+		searchScan.nextLine();
+		if (searchType == 1) {
+			System.out.println("Searching by course code \nEnter the course code you would like to search for");
+
+			String code = searchScan.nextLine().toUpperCase(); //matches all to the format in the database
+			//check for valid user input
+			ArrayList<Course> searchResults = searchCoursesByCode(code);
+			System.out.println("Search results:");
+			for (int i = 1; i <= searchResults.size(); i++) {
+				System.out.println(i + ". " +searchResults.get(i - 1));
+			}
+		}
+	}
+
+	public static ArrayList<Course> searchCoursesByCode(String code) {
+		ArrayList<Course> result = new ArrayList<>();
+		for (Course c : courses) {
+			if (c.getCode().contains(code)) {
+				result.add(c);
+			}
+		}
+		return result;
+	}
 	public ArrayList<Course> searchCoursesByKeyword(String keyword) {return null;}
 	public ArrayList<Course> dayFilter(ArrayList<Course> courses, ArrayList<String> days) {return null;}
 	public ArrayList<Course> timeFilter(ArrayList<Course> courses, ArrayList<String> times) {return null;}
@@ -49,12 +75,32 @@ public class UserInterface {
 
 	}
 
+	public static void test1() {
+		courses = new ArrayList<Course>();
+
+		Course c1 = new Course("MUSI 102", "MUSIC HISTORY II", "9:00 AM", "9:50 AM", 'A', "MWF");
+		Course c2 = new Course("MUSI 102", "MUSIC HISTORY II", "9:00 AM", "9:50 AM", 'B', "MWF");
+		Course c3 = new Course("COMP 141", "INTRO TO PROGRAM", "11:00 AM", "11:50 AM", 'A', "MWF");
+
+		System.out.println(c1);
+
+		courses.add(c1);
+		courses.add(c2);
+		courses.add(c3);
+
+
+		System.out.println(courses);
+		System.out.println(searchCoursesByCode("COMP") + "\n");
+
+		consoleSearch();
+	}
+
 	public static void main(String args[]) {
-		Test test = new Test();
-		test.test1();
-		menuNav(0);
-		Schedule tests = new Schedule(courses, "Spring 2023");
-		tests.displaySchedule();
+//		menuNav(0);
+//		Schedule tests = new Schedule(courses, "Spring 2023");
+//		tests.displaySchedule();
+
+		test1();
 	}
 
 
