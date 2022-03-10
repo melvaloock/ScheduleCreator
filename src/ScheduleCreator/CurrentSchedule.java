8c6d862796ece41335b505d08bfb77fefadb9050
John Burke <burkejr19@gcc.edu>
2022-03-10 16:22:43 -0500

    completed clearSchedule, addCourse, removeCourse

--- src/ScheduleCreator/CurrentSchedule.java
+++ src/ScheduleCreator/CurrentSchedule.java
@@ -12,8 +12,25 @@ public class CurrentSchedule extends Schedule {
 		super();
 	}
 
-	public void clearSchedule() {}
-	public void addCourse(Course c) {}
-	public void removeCourse(Course c) {}
-	public boolean conflictsWith(Course c) {return false;}
+	public void clearSchedule() {
+		courseList = new ArrayList<Course>();
+	}
+
+	public void addCourse(Course c) {
+		courseList.add(c);
+	}
+
+	/**
+	 * Removes course c from courseList. If c is not in courseList, returns false.
+	 * @param c
+	 * @return true if course successfully removed, false otherwise
+	 */
+	public boolean removeCourse(Course c) {
+		return courseList.remove(c);
+	}
+
+	public boolean conflictsWith(Course c) {
+		return false;
+	}
+
 }
