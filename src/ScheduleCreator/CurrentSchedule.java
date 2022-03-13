120a651743419abea2385045746860348eefd1ec
John Burke <burkejr19@gcc.edu>
2022-03-13 13:19:35 -0400

    started conflictsWith method

--- src/ScheduleCreator/CurrentSchedule.java
+++ src/ScheduleCreator/CurrentSchedule.java
@@ -28,7 +28,32 @@ public class CurrentSchedule extends Schedule {
 		courseList.remove(c);
 	}
 
+	/**
+	 * returns true if Course c has a time conflict with any of the courses in the current schedule
+	 * @param c
+	 * @return boolean
+	 */
 	public boolean conflictsWith(Course c) {
+		// if c is NULL, then it will have no conflicts
+		if (c.getStartTime() == null){
+			return false;
+		}
+
+		// parse Course c time
+		String[] times = c.getStartTime().split(":");
+		double cStart = Integer.valueOf(times[0]) + Integer.valueOf(times[1])/60;
+
+		// check if each course has a conflict
+		for (Course current: courseList) {
+			// check current course to see if it's NULL
+			if (current.getStartTime() == null){
+				continue;
+			}
+
+			// check startTime
+
+		}
+
 		return false;
 	}
 
