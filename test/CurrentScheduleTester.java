74a1dd7ddb7076f13eb102f9d656170996e5f468
John Burke <burkejr19@gcc.edu>
2022-03-13 17:13:41 -0400

    created JUnit tester for CurrentSchedule

---
+++ test/CurrentScheduleTester.java
@@ -0,0 +1,25 @@
+import ScheduleCreator.Course;
+import ScheduleCreator.CurrentSchedule;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CurrentScheduleTester {
+
+    @Test
+    public void addCourse(){
+        CurrentSchedule cs = new CurrentSchedule(new ArrayList<Course>());
+        Course c1 = new Course("MUSI 102", "MUSIC HISTORY II", "9:00 AM", "9:50 AM", 'A', "MWF");
+        cs.addCourse(c1);
+
+        ArrayList<Course> courseList = cs.getCourseList();
+
+        // should have 1 course
+        Assert.assertEquals(1, courseList.size());
+
+        // should be the same course
+        Assert.assertEquals(c1, courseList.get(0));
+    }
+
+}
