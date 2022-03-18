d1ef9c12cab38f157ec20051e0f4b619f87e2306
Kevin Hamby <82542818+krhamby@users.noreply.github.com>
2022-03-18 17:14:41 -0400

    Added addCourse and parseCourses

--- src/ScheduleCreator/Database.java
+++ src/ScheduleCreator/Database.java
@@ -1,7 +1,11 @@
 package ScheduleCreator;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
+import java.util.Scanner;
+import java.time.LocalTime;
 
 public class Database { 
     private Connection conn;
@@ -18,7 +22,59 @@ public class Database {
         } 
     }
 
+    public void addCourse(int CourseID, String courseCode, String courseName, String startTime, String endTime, String weekday, int enrollment, int capacity) {
+        try {
+            PreparedStatement pstmtCheck = conn.prepareStatement("SELECT * FROM course WHERE CourseID = ?");
+            pstmtCheck.setInt(1, CourseID);
+            ResultSet rstCheck = pstmtCheck.executeQuery();
+
+            if (rstCheck.next()) { 
+                throw new SQLException("That course already exists.");
+            }
+
+            PreparedStatement insertStmt = conn.prepareStatement("INSERT INTO course values(?, ?, ?, ?, ?, ?, ?, ?)");
+            insertStmt.setInt(1, CourseID);
+            insertStmt.setString(2, courseCode);
+            insertStmt.setString(3, courseName);
+            insertStmt.setString(4, weekday);
+            insertStmt.setString(5, startTime);
+            insertStmt.setString(6, endTime);
+            insertStmt.setInt(7, enrollment);
+            insertStmt.setInt(8, capacity);
+            int rows = insertStmt.executeUpdate();
+
+            if (rows > 0) {
+                System.out.println("Insertion successful!");
+            }
+
+            pstmtCheck.close();
+            rstCheck.close(); 
+            insertStmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+
+    public void processCourses(String csvFilename) {
+        try {
+            Scanner csvScanner = new Scanner(new File(csvFilename));
+            String[] vars;
+            int courseID = 1;
+            while (csvScanner.hasNextLine()) {
+                vars = csvScanner.nextLine().split(";");
+                addCourse(courseID, vars[0], vars[2], vars[3], vars[4], vars[5], Integer.parseInt(vars[8]), Integer.parseInt(vars[9]));
+                courseID++;
+            } 
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String args[]) {
         Database db = new Database("root", "password", "sys");
+        db.processCourses("CourseDB_WithFictionalCapacities.csv");
     }
 }
