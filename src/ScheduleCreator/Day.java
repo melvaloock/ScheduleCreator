17e2a5f5a1e177d0e110abe0193ced876c5df177
Kevin Hamby <82542818+krhamby@users.noreply.github.com>
2022-03-23 18:46:18 -0400

    added the necessary framework to get an account and all its information from the database; the method to return a student/account needs completed

---
+++ src/ScheduleCreator/Day.java
@@ -0,0 +1,23 @@
+package ScheduleCreator;
+
+public enum Day {
+
+    M, T, W, R, F, NULL;
+
+    public static Day getDay(char c) {
+        switch (c) {
+            case 'M':
+                return Day.M;
+            case 'T':
+                return Day.T;
+            case 'W':
+                return Day.W;
+            case 'R':
+                return Day.T;
+            case 'F':
+                return Day.F;
+            default:
+                return null;
+        }
+    }
+}
