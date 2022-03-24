23534d3d38f0294f3e1463fd12ea1910b1c7a7bc
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2022-03-24 16:04:15 -0400

    Date and Time push not complete

--- src/ScheduleCreator/DateAndTime.java
+++ src/ScheduleCreator/DateAndTime.java
@@ -1,5 +1,5 @@
 package ScheduleCreator;
-import java.text.DateFormat;
+ import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,10 +13,15 @@ public class DateAndTime {
     private final String DAYOFWEEKFORMAT = "u";
     private Date time1;
     private Date time2;
+    private ArrayList<Date> scheduleTime;
     private ArrayList<Date> daysOfWeek;
-    private Date combined;
 
     public DateAndTime(){
+    }
+
+    public void setTime(String scheduleTime) throws ParseException {
+        SimpleDateFormat inputParser = new SimpleDateFormat(TIMEFORMAT, Locale.US);
+        this.scheduleTime.add(inputParser.parse(scheduleTime));
     }
 
     public void setTime(String time1, String time2) throws ParseException {
@@ -25,8 +30,8 @@ public class DateAndTime {
         this.time2 = inputParser.parse(time2);
     }
 
-    public long timeDifference(){
-        return (time2.getTime() - time1.getTime());
+    public double timeDifference(){
+        return (((double)(time2.getTime() - time1.getTime())/60000)/60);
     }
 
     public void setDaysOfWeek(ArrayList<Character> daysOfWeek) throws ParseException {
@@ -39,5 +44,41 @@ public class DateAndTime {
     public ArrayList<Date> getDaysOfWeek() {
         return daysOfWeek;
     }
+
+
+
+    //    It returns the value 0 if the argument Date is equal to this Date.
+    //    It returns a value less than 0 if this Date is before the Date argument.
+    //    It returns a value greater than 0 if this Date is after the Date argument.
+    public ArrayList<ArrayList<Integer>> locOfClass(String time1, String time2, ArrayList<Character> daysOfWeek) throws ParseException {
+        ArrayList<ArrayList<Integer>> loc;
+        setTime(time1,time2);
+
+        String[] times = {"8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM",
+                "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM"};
+        for (String tim: times){
+            setTime(tim);
+        }
+        for(int x = 0; x < times.length; x++){
+            if(scheduleTime.get(x).compareTo(this.time1) == 0){
+                if(timeDifference() > 1){
+
+                }
+            }
+        }
+
+        return null;
+    }
+
+//    public ArrayList<Integer> findDayOfWeek(ArrayList<Character> daysOfWeek) throws ParseException {
+//        setDaysOfWeek(daysOfWeek);
+//        int[] days = {1,2,3,4,5};
+//        ArrayList<Integer> dayLoc;
+//        for(int x = 0; x < daysOfWeek.size(); x++){
+//            if(this.daysOfWeek.get(x) == days[x]){
+//
+//            }
+//        }
+//    }
 
 }
