bb08ce9cbd38e7afaef648f4da1763bae6a39265
John Burke <burkejr19@gcc.edu>
2022-03-24 15:18:05 -0400

    refactored PasswordHashingTester

--- src/ScheduleCreator/AccountCreateLoginTester.java
+++ src/ScheduleCreator/AccountCreateLoginTester.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 
-public class PasswordHashingTester {
+public class AccountCreateLoginTester {
 
     @Test
     public void passwordHashWorks() throws PasswordStorage.CannotPerformOperationException, PasswordStorage.InvalidHashException {
