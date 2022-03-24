372a67ef34b627191904aa69c26cefbf5f50452f
John Burke <burkejr19@gcc.edu>
2022-03-24 19:45:34 -0400

    createAccount JUnit test

--- src/ScheduleCreator/AccountCreateLoginTester.java
+++ src/ScheduleCreator/AccountCreateLoginTester.java
@@ -3,8 +3,24 @@ package ScheduleCreator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.sql.*;
+import java.util.Properties;
+
 
 public class AccountCreateLoginTester {
+
+    private Database db = new Database("root", "EnuzPkHDO29J6gCH", "schedule_creator_db");
+    private Connection conn;
+
+    private void dbSetUp() throws SQLException {
+        String schema = "schedule_creator_db";
+        String username = "root";
+        String password = "EnuzPkHDO29J6gCH";
+        Properties info = new Properties();
+        info.put("user", username);
+        info.put("password", password);
+        conn = DriverManager.getConnection("jdbc:mysql://34.121.27.151/" + schema, info);
+    }
 
     @Test
     public void passwordHashWorks() throws PasswordStorage.CannotPerformOperationException, PasswordStorage.InvalidHashException {
@@ -12,6 +28,28 @@ public class AccountCreateLoginTester {
         String hash = PasswordStorage.createHash(pass);
         Assert.assertTrue(PasswordStorage.verifyPassword(pass, hash));
         Assert.assertFalse(PasswordStorage.verifyPassword("wrong", hash));
+    }
+
+    @Test
+    public void canCreateAccount() throws SQLException, PasswordStorage.CannotPerformOperationException {
+        dbSetUp();
+
+        // add account
+        String email = "createAccountTest@email.com";
+        String password = "create";
+        db.addAccount(email, password);
+
+        PreparedStatement stmnt = conn.prepareStatement("SELECT * FROM account WHERE UserEmail = ?");
+        stmnt.setString(1, email);
+        ResultSet rstCheck = stmnt.executeQuery();
+
+        // if has next, then account exists with the info given
+        Assert.assertTrue(rstCheck.next());
+
+        // remove account from db
+        stmnt = conn.prepareStatement("DELETE FROM account WHERE UserEmail = ?");
+        stmnt.setString(1, email);
+        stmnt.executeUpdate();
     }
 
 }
