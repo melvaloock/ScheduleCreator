7c0d85a670c45aac5207a13975198623aa8f8220
Kevin Hamby <82542818+krhamby@users.noreply.github.com>
2022-04-23 13:11:58 -0400

    syncing

--- src/main/java/sleeplessdevelopers/schedulecreator/ApplicationController.java
+++ src/main/java/sleeplessdevelopers/schedulecreator/ApplicationController.java
@@ -2,12 +2,18 @@ package sleeplessdevelopers.schedulecreator;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class ApplicationController {
     
     @GetMapping("/")
-    public String index() {
-        return "Hello World!";
+    public ModelAndView index() {
+        return new ModelAndView("index.html");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "pass";
     }
 }
