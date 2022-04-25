f1b9a3e98e6a6b5c030c7fdf94fdfefead14a541
melvaloock <melvaloock@gmail.com>
2022-04-25 06:00:42 -0400

    about page

--- src/main/java/sleeplessdevelopers/schedulecreator/ApplicationController.java
+++ src/main/java/sleeplessdevelopers/schedulecreator/ApplicationController.java
@@ -32,7 +32,6 @@ public class ApplicationController extends UserInterface {
             //next 3 lines for testing purposes
 //            RecommendedSchedule rs = new RecommendedSchedule("Computer Science (BS)", 2024, db);
 //            currentStudent.setCurrentSchedule(rs.makeCurrentSchedule());
-//            currentStudent.addRecommendedSchedule();
             model.addAttribute("schedule", currentStudent.getCurrentSchedule());
             return "ScheduleView.html";
         }
@@ -70,7 +69,12 @@ public class ApplicationController extends UserInterface {
 
     @GetMapping("/auto-schedule")
     public String getAutoSchedule() {
-        return "AutoSchedule.html";
+        if (currentStudent == null) {
+            return "redirect:/login";
+            //TODO: add error message
+        } else {
+            return "AutoSchedule.html";
+        }
     }
     
     @GetMapping("/login")
@@ -111,8 +115,13 @@ public class ApplicationController extends UserInterface {
 
     @GetMapping("/search")
     public String getSearch(Model model) {
-        model.addAttribute("searchForm", new SearchForm());
-        return "CourseSearch.html";
+        if (currentStudent == null) {
+            return "redirect:/login";
+            //TODO: add error message
+        } else {
+            model.addAttribute("searchForm", new SearchForm());
+            return "CourseSearch.html";
+        }
     }
 
     @PostMapping("/search")
