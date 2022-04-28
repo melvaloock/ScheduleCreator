c00ceaadf817909b0b65ef878409f147084728c0
melvaloock <melvaloock@gmail.com>
2022-04-27 23:36:26 -0400

    killing unnecessary files

--- static/scripts/toast.js
+++
@@ -1,5 +0,0 @@
-var toasts = [].slice.call(document.querySelectorAll('.toast'))
-var toastList = toasts.map(function (toast) {
-    return new bootstrap.Toast(toast)
-})
-toastList.forEach(toast => toast.show())
